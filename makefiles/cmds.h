70c998f53cd63dafe0087f1ce796f0b552026ab4
ambitiousCC <1297571059@qq.com>
2020-11-10 21:13:32 +0800

    #demo test1

---
+++ makefiles/cmds.h
@@ -0,0 +1,10 @@
+#ifndef _CMD_H
+#define _CMD_H
+int fun_cd(char** char_list);
+int fun_pwd(char** char_list);
+int fun_exit(char** char_list);
+int fun_echo(char** char_list);
+int fun_touch(char** char_list);
+int fun_mkdir(char** char_list);
+int fun_remove_dir(char** char_list);
+#endif
---
+++ makefiles/ls.h
@@ -0,0 +1,7 @@
+#ifndef _LS_H
+#define _LS_H
+char *get_mode(mode_t m, char *str);
+int _time(int year);
+void time_ch(time_t num);
+int fun_ls(char** char_list);
+#endif
